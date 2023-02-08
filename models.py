30d8a605fdabae7dff03d67f9dffc3cf1c66236c
DragonDmoney <Pierre>
2023-02-07 18:52:27 -0600

    refactor and add claims

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from peewee import *
 
-# db = SqliteDatabase('db', pragmas={'journal_mode': 'wal'})
-db = PostgresqlDatabase('root', user='root')
+db = SqliteDatabase('db', pragmas={'journal_mode': 'wal'})
+#  db = PostgresqlDatabase('root', user='root')
 
 class State(Model):
     ethereum_height = IntegerField()
@@ -24,11 +24,14 @@ class Claim(Model):
     chainflip_hash = CharField(null = True)
     initiated_height = IntegerField(null = True) # height the claim was submitted on chainflip
     completed_height = IntegerField(null = True) # the height the claim was completed on eth
+    expired_height = IntegerField(null = True) # the height the claim expired on chainflip (if it did)
     amount = FloatField()
     #  claim_signature = CharField(null = True)
     msg_hash = CharField(null = True)
     start_time = IntegerField(null = True)
-    address = CharField()
+    expiry_time = IntegerField(null = True)
+    node = CharField()
+    staker = CharField(null = True)
 
     class Meta:
         database = db
@@ -42,7 +45,7 @@ class Validator(Model):
     class Meta:
         database = db
 
-db.create_tables([State, Stake, Validator])
+db.create_tables([State, Stake, Validator, Claim])
 
 if State.select().count() == 0:
     State.create(ethereum_height=0, chainflip_height=0)
